92d927d9971afb7143932ee4efaed5fa9b8e59bd
Leticia <anna.fernandes1759@gmail.com>
2026-07-20 22:33:40 -0300

    feat: create history page structure

--- frontend/src/pages/History/History.jsx
+++ frontend/src/pages/History/History.jsx
@@ -1,5 +1,8 @@
 export default function History(){
     return(
-        <h1>history page</h1>
+        <>
+            <h1>history page</h1>
+            <p>This is your workout history</p>
+        </>
     )
 }
